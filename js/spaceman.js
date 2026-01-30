32bf4defc7c631c741da54485f6a7c21ed073e2b
geda0 <geda071@gmail.com>
2026-01-29 19:18:46 -0700

    spaceman v2

--- js/spaceman.js
+++ js/spaceman.js
@@ -9,6 +9,10 @@ class Spaceman {
     this.typingTimeout = null;
     this.messageTimeout = null;
     this.idleTimer = null;
+    this.clickCount = 0;
+    this.clickTimer = null;
+    this.lastMousePos = { x: 0, y: 0 };
+    this.isMouseInViewport = true;
 
     this.init(dataUrl);
   }
@@ -25,7 +29,9 @@ class Spaceman {
 
     this.render();
     this.bindEvents();
+    this.bindInteractivity();
     this.startMessageCycle();
+    this.startIdleAnimations();
   }
 
   render() {
@@ -45,14 +51,14 @@ class Spaceman {
             <div class="visor">
               <div class="visor-reflection"></div>
               <div class="eyes">
-                <span class="eye left"></span>
-                <span class="eye right"></span>
+                <span class="eye left-eye"></span>
+                <span class="eye right-eye"></span>
               </div>
             </div>
           </div>
+          <div class="arm left-arm"></div>
+          <div class="arm right-arm"></div>
           <div class="torso">
-            <div class="arm left-arm"></div>
-            <div class="arm right-arm"></div>
             <div class="chest-panel">
               <span class="light light-1"></span>
               <span class="light light-2"></span>
@@ -60,16 +66,18 @@ class Spaceman {
           </div>
           <div class="jetpack">
             <div class="pack"></div>
+          </div>
+          <div class="legs">
+            <div class="leg left-leg"></div>
+            <div class="leg right-leg"></div>
+          </div>
+          <div class="flames-container">
             <div class="flame left-flame">
               <div class="flame-inner"></div>
             </div>
             <div class="flame right-flame">
               <div class="flame-inner"></div>
             </div>
-          </div>
-          <div class="legs">
-            <div class="leg left-leg"></div>
-            <div class="leg right-leg"></div>
           </div>
         </div>
       </div>
@@ -178,6 +186,158 @@ class Spaceman {
     }
   }
 
+  bindInteractivity() {
+    const spaceman = this.elements.spaceman;
+    const helmet = spaceman.querySelector('.helmet');
+    const jetpack = spaceman.querySelector('.jetpack');
+    const eyes = spaceman.querySelectorAll('.eye');
+
+    // --- Eye Tracking ---
+    document.addEventListener('mousemove', (e) => {
+      this.lastMousePos = { x: e.clientX, y: e.clientY };
+      this.updateEyePosition(e.clientX, e.clientY);
+    });
+
+    // --- Mouse Leave Viewport ---
+    document.addEventListener('mouseleave', () => {
+      this.isMouseInViewport = false;
+      eyes.forEach(eye => eye.classList.add('searching'));
+    });
+
+    document.addEventListener('mouseenter', () => {
+      this.isMouseInViewport = true;
+      eyes.forEach(eye => eye.classList.remove('searching'));
+    });
+
+    // --- Helmet Click ---
+    helmet.addEventListener('click', (e) => {
+      e.stopPropagation();
+      helmet.classList.add('flash');
+      this.triggerBlink();
+      this.typeMessage("*visor sparkles* ✨", 30);
+      setTimeout(() => helmet.classList.remove('flash'), 300);
+      setTimeout(() => this.startMessageCycle(), 2000);
+    });
+
+    // --- Jetpack Click ---
+    jetpack.addEventListener('click', (e) => {
+      e.stopPropagation();
+      const spacemanBody = spaceman.querySelector('.spaceman-body');
+      spacemanBody.classList.add('burst');
+      this.typeMessage("WHOOOOSH! 🔥", 20);
+      setTimeout(() => spacemanBody.classList.remove('burst'), 400);
+      setTimeout(() => this.startMessageCycle(), 2000);
+    });
+
+    // --- Rapid Click Detection ---
+    spaceman.addEventListener('click', () => {
+      this.clickCount++;
+
+      clearTimeout(this.clickTimer);
+      this.clickTimer = setTimeout(() => {
+        if (this.clickCount >= 5) {
+          this.triggerDizzy();
+        }
+        this.clickCount = 0;
+      }, 1000);
+    });
+  }
+
+  updateEyePosition(mouseX, mouseY) {
+    const spaceman = this.elements.spaceman;
+    const rect = spaceman.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 4;
+
+    const deltaX = mouseX - centerX;
+    const deltaY = mouseY - centerY;
+
+    let direction = '';
+    const threshold = 50;
+
+    if (Math.abs(deltaY) < threshold && Math.abs(deltaX) < threshold) {
+      direction = '';
+    } else if (deltaY < -threshold && deltaX < -threshold) {
+      direction = 'look-top-left';
+    } else if (deltaY < -threshold && deltaX > threshold) {
+      direction = 'look-top-right';
+    } else if (deltaY > threshold && deltaX < -threshold) {
+      direction = 'look-bottom-left';
+    } else if (deltaY > threshold && deltaX > threshold) {
+      direction = 'look-bottom-right';
+    } else if (deltaY < -threshold) {
+      direction = 'look-up';
+    } else if (deltaY > threshold) {
+      direction = 'look-down';
+    } else if (deltaX < -threshold) {
+      direction = 'look-left';
+    } else if (deltaX > threshold) {
+      direction = 'look-right';
+    }
+
+    const eyes = spaceman.querySelectorAll('.eye');
+    const lookClasses = [
+      'look-left', 'look-right', 'look-up', 'look-down',
+      'look-top-left', 'look-top-right',
+      'look-bottom-left', 'look-bottom-right'
+    ];
+
+    eyes.forEach(eye => {
+      lookClasses.forEach(cls => eye.classList.remove(cls));
+      if (direction) {
+        eye.classList.add(direction);
+      }
+    });
+  }
+
+  triggerBlink() {
+    const eyes = this.elements.spaceman.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+      eye.classList.add('blink');
+      setTimeout(() => eye.classList.remove('blink'), 150);
+    });
+  }
+
+  triggerDizzy() {
+    const spaceman = this.elements.spaceman;
+    spaceman.classList.add('dizzy');
+    this.typeMessage("Woah... so many clicks! 😵‍💫", 30);
+    setTimeout(() => spaceman.classList.remove('dizzy'), 800);
+    setTimeout(() => this.startMessageCycle(), 3000);
+  }
+
+  startIdleAnimations() {
+    // Random blink every 3-7 seconds
+    const scheduleBlink = () => {
+      const delay = 3000 + Math.random() * 4000;
+      setTimeout(() => {
+        if (this.isMouseInViewport) {
+          this.triggerBlink();
+        }
+        scheduleBlink();
+      }, delay);
+    };
+    scheduleBlink();
+
+    // Random wave every 15-30 seconds
+    const scheduleWave = () => {
+      const delay = 15000 + Math.random() * 15000;
+      setTimeout(() => {
+        if (this.state === 'idle') {
+          this.triggerWave();
+        }
+        scheduleWave();
+      }, delay);
+    };
+    scheduleWave();
+  }
+
+  triggerWave() {
+    const spaceman = this.elements.spaceman;
+    spaceman.classList.add('wave');
+    setTimeout(() => spaceman.classList.remove('wave'), 1800);
+  }
+
   getDefaultData() {
     return {
       states: {
@@ -194,6 +354,7 @@ class Spaceman {
     clearTimeout(this.typingTimeout);
     clearTimeout(this.messageTimeout);
     clearTimeout(this.idleTimer);
+    clearTimeout(this.clickTimer);
     this.container.innerHTML = '';
   }
 }
